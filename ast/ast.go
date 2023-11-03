a2480336996a7b1be082386e8db1c59ed6509b2a
Bobby <bobbyskhs@gmail.com>
2023-11-03 14:23:43 +0000

    ast:add expression statement

--- ast/ast.go
+++ ast/ast.go
@@ -63,3 +63,14 @@ func (rs *ReturnStatement) statementNode() {}
 func (rs *ReturnStatement) TokenLiteral() string {
 	return rs.Token.Literal
 }
+
+// ExpressionStatement represents an expression statement.
+type ExpressionStatement struct {
+	Token      tokens.Token // the first token of the expression
+	Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode() {}
+func (es *ExpressionStatement) TokenLiteral() string {
+	return es.Token.Literal
+}
