195c72b225c6f171011dfdde550f63cb409b7249
Bobby <bobbyskhs@gmail.com>
2023-11-02 21:29:15 -0400

    base:ast

---
+++ ast/ast.go
@@ -0,0 +1,54 @@
+package ast
+
+import "mana/tokens"
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+// TokenLiteral returns the literal value of the token associated with this
+// node. This is used only for debugging and testing.
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	}
+	return ""
+}
+
+// LetStatement represents a let statement.
+type LetStatement struct {
+	Token tokens.Token // the token.LET token
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode() {}
+func (ls *LetStatement) TokenLiteral() string {
+	return ls.Token.Literal
+}
+
+// Identifier represents an identifier.
+type Identifier struct {
+	Token tokens.Token // the token.IDENT token
+	Value string
+}
+
+func (i *Identifier) expressionNode() {}
+func (i *Identifier) TokenLiteral() string {
+	return i.Token.Literal
+}
