c430bfcae07489ad52ae65cadee581be72dd35d1
Bobby <bobbyskhs@gmail.com>
2024-03-04 08:38:06 -0500

    error handling

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -3,6 +3,7 @@ package evaluator
 import (
 	"mana/ast"
 	"mana/object"
+  "fmt"
 )
 
 var (
@@ -33,11 +34,20 @@ func Eval(node ast.Node) object.Object {
 
 	case *ast.PrefixExpression:
 		right := Eval(node.Right)
+    if isError(right) {
+      return right
+    }
 		return evalPrefixExpression(node.Operator, right)
 
 	case *ast.InfixExpression:
 		left := Eval(node.Left)
+    if isError(left) {
+      return left
+    }
 		right := Eval(node.Right)
+    if isError(right) {
+      return right
+    }
 		return evalInfixExpression(node.Operator, left, right)
 
 	case *ast.IfExpression:
@@ -45,6 +55,9 @@ func Eval(node ast.Node) object.Object {
 
 	case *ast.ReturnStatement:
 		val := Eval(node.ReturnValue)
+    if isError(val) {
+      return val
+    }
 		return &object.ReturnValue{Value: val}
 	}
 
@@ -57,9 +70,12 @@ func evalProgram(program *ast.Program) object.Object {
 	for _, statement := range program.Statements {
 		result = Eval(statement)
 
-		if returnValue, ok := result.(*object.ReturnValue); ok {
-			return returnValue.Value
-		}
+    switch result := result.(type) {
+    case *object.ReturnValue:
+      return result.Value
+    case *object.Error:
+      return result
+    }
 	}
 
 	return result
@@ -71,9 +87,12 @@ func evalBlockStatement(block *ast.BlockStatement) object.Object {
 	for _, statement := range block.Statements {
 		result = Eval(statement)
 
-		if result != nil && result.Type() == object.RETURN_VALUE_OBJ {
-			return result
-		}
+    if result != nil {
+      rt := result.Type()
+      if rt == object.RETURN_VALUE_OBJ || rt == object.ERROR_OBJ {
+        return result
+      }
+    }
 	}
 
 	return result
@@ -93,7 +112,7 @@ func evalPrefixExpression(operator string, right object.Object) object.Object {
 	case "-":
 		return evalMinusPrefixOperatorExpression(right)
 	default:
-		return NULL
+    return newError("unknown operator: %s%s", operator, right.Type())
 	}
 }
 
@@ -105,8 +124,10 @@ func evalInfixExpression(operator string, left, right object.Object) object.Obje
 		return nativeBoolToBooleanObject(left == right)
 	case operator == "!=":
 		return nativeBoolToBooleanObject(left != right)
+  case left.Type() != right.Type():
+    return newError("type mismatch: %s %s %s", left.Type(), operator, right.Type())
 	default:
-		return NULL
+		return newError("unknown operator: %s %s %s", left.Type(), operator, right.Type()) 
 	}
 }
 
@@ -125,7 +146,7 @@ func evalBangOperatorExpression(right object.Object) object.Object {
 
 func evalMinusPrefixOperatorExpression(right object.Object) object.Object {
 	if right.Type() != object.INTEGER_OBJ {
-		return NULL
+		return newError("unknown operator: -%s", right.Type())
 	}
 
 	value := right.(*object.Integer).Value
@@ -157,12 +178,15 @@ func evalIntegerInfixExpression(operator string, left, right object.Object) obje
 	case "!=":
 		return nativeBoolToBooleanObject(leftValue != rightValue)
 	default:
-		return NULL
+		return newError("unknown operator: %s %s %s", left.Type(), operator, right.Type())
 	}
 }
 
 func evalIfExpression(ie *ast.IfExpression) object.Object {
 	condition := Eval(ie.Condition)
+  if isError(condition) {
+    return condition
+  }
 	if isTruthy(condition) {
 		return Eval(ie.Consequence)
 	} else if ie.Alternative != nil {
@@ -183,4 +207,15 @@ func isTruthy(obj object.Object) bool {
 	default:
 		return true
 	}
+}
+
+func newError(format string, a ...interface{}) *object.Error {
+  return &object.Error{Message: fmt.Sprintf(format, a...)}
+}
+
+func isError(obj object.Object) bool {
+  if obj != nil {
+    return obj.Type() == object.ERROR_OBJ
+  }
+  return false
 }
--- object/object.go
+++ object/object.go
@@ -9,6 +9,7 @@ const (
 	BOOLEAN_OBJ      = "BOOLEAN"
 	NULL_OBJ         = "NULL"
 	RETURN_VALUE_OBJ = "RETURN_VALUE"
+  ERROR_OBJ        = "ERROR"
 )
 
 type Object interface {
@@ -28,6 +29,10 @@ type Null struct{}
 
 type ReturnValue struct {
 	Value Object
+}
+
+type Error struct {
+  Message string
 }
 
 func (i *Integer) Type() ObjectType {
@@ -61,3 +66,6 @@ func (rv *ReturnValue) Type() ObjectType {
 func (rv *ReturnValue) Inspect() string {
 	return rv.Value.Inspect()
 }
+
+func (e *Error) Type() ObjectType { return ERROR_OBJ }
+func (e *Error) Inspect() string { return "ERROR:" + e.Message }
